9e1fdfabe41a61fb8ef7669ffa8759863e050f9a
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-11-02 11:37:29 -0500

    Translated given js code to jquery

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//JQuery Translation
+function newItem() {
+  // Adding a new item to the list of items
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('You must write something!');
+  } else {
+    $('#list').append(li);
+  }
+
+  // Crossing out an item from the list of items
+  function crossOut() {
+    li.toggleClass('strike');
+  }
+
+  li.on('dblclick', crossOut);
+
+  // Adding the delete button "X"
+  let crossOutButton = $('<button class="crossOutButton"></button>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+
+  // Adding CLASS DELETE (DISPLAY: NONE) from the css
+  function deleteListItem () {
+    li.addClass('delete');
+  }
+
+  // Reordering the items
+  $('#list').sortable();
+
+}
